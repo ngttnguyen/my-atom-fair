0d7c17f489a1ed2b8dc9fe2140f364c5cc706468
Hara Nguyen <ngttnguyen@gmail.com>
2021-07-03 01:18:31 +0700

    update

--- Nguyen_Bank_Marketing_Streamlit.py
+++ Nguyen_Bank_Marketing_Streamlit.py
@@ -5,16 +5,59 @@ import json
 import pickle
 from matplotlib import pyplot as plt
 import seaborn as sns
+pd.plotting.register_matplotlib_converters()
+import warnings
+warnings.filterwarnings("ignore")
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import precision_recall_curve
+from sklearn import metrics
+from xgboost import XGBClassifier
+import scikitplot as skplt
+import matplotlib.pyplot as plt
 
-def load_data(file_path):
-    file_path = "data/bank-additional-full.csv"
-    data = pd.read_csv(file_path,sep = ";")
-    return data
+def transform_pdays(val):
+    transform_dict = {999:'not_previously_contacted',7: 'over_a_week',0:'within_a_week'}
+    for key in transform_dict.keys():
+        if (val >= key):
+            return transform_dict[key]
+        
+### processing input data
+def process_input_client(client_df):
+    num_cols = marketing_df.dtypes[marketing_df.dtypes != 'object'].index.tolist()
+    cat_cols = [col for col in marketing_df.dtypes[marketing_df.dtypes == 'object'].index.tolist() if col != 'y']
 
-def convert_str_to_float(mydict):
-    for item in mydict.keys():
-        mydict[item] = float(mydict[item])
-    return mydict
+    missing_val = 'unknown'
+    un_replaced_lst = ['default']
+    for col in cat_cols:
+        if (col in un_replaced_lst):
+            continue
+        replaced_val = marketing_df[col].mode().values.tolist()[0]
+        client_df[col] = client_df[col].apply(lambda val: replaced_val if val == missing_val else val)
+
+
+    for col in num_cols:
+        if col == 'pdays':
+            client_df[col] = client_df[col].map(transform_pdays)
+            cat_cols = cat_cols +['pdays'] 
+            continue
+
+        if col == 'campaign':
+            replaced_val = 6
+        else:
+            replaced_val = marketing_df[col].quantile(0.95)   
+
+        client_df[col] = client_df[col].apply(lambda val: replaced_val if val > replaced_val else val)
+
+    ## onehot_encoding:
+    cat_cols = [col for col in client_df.dtypes[client_df.dtypes == 'object'].index.tolist()]  
+    labelencoder = LabelEncoder()
+    for column in cat_cols:
+        client_df[column] = labelencoder.fit_transform(client_df[column])
+        
+    return(client_df)  
+ 
 def get_cat_cols_val(data):
     unique_dict= {}
     cat_cols = data.dtypes[data.dtypes == 'object'].index
@@ -24,49 +67,70 @@ def get_cat_cols_val(data):
     return unique_dict
 
 # Initial setup
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 
 #### L O A D  Data
 data_file_path = "data/bank-additional-full.csv"
 marketing_df = pd.read_csv(data_file_path,sep = ";")
 
-#### L O A D  Model & coefs 
-coef_file_path = ("model/model_coefs.json") 
-with open(coef_file_path) as infile:
-    coef_dict = json.load(infile)
-coef_dict = convert_str_to_float(coef_dict)
-
 model_file_path = "model/pkl_model.pkl"
 model = pickle.load(open(model_file_path, 'rb'))
-    
+
+
 
 #### M A I N  Function
 def main():
+    
+    client_df_ok = pd.read_csv("client_df.csv", index_col = 'Unnamed: 0')
+    
+    # client= process_input_client(client_df_ok )
+    # print(client)
+    # X_client_test = client.drop(['y'], axis = 1) 
+    # X_client_test = StandardScaler().fit_transform(X_client_test)
+    # print(X_client_test)
+    # y_client_pred = model.predict(X_client_test)
+    # print(y_client_pred)
     
     # unique_dict = get_cat_cols_val(marketing_df) #contain unique values of features
     
     st.title('Banking TeleMarketing prediction')
     st.sidebar.markdown('## XGBoost Classifier')
-    
-  
     st.markdown("Input client's information: ")
-    #['age', 'job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'day_of_week', \
-        # 'duration', 'campaign', 'pdays', 'previous', 'poutcome', 'emp.var.rate', 'cons.price.idx', \
-            # 'cons.conf.idx', 'euribor3m', 'nr.employed']
-    cat_cols = marketing_df.dtypes[marketing_df.dtypes == 'object'].index
-    for col in cat_cols:
 
-        col_option = marketing_df[col].unique().tolist()
-        col_mode = marketing_df[col].mode().tolist()[0]
-        col_selected = col_option.index(col_mode)
-        col_option_id = st.selectbox('Choose '+col.capitalize()+':',options = col_option, index = col_selected)
+    target = 'y'
+    tam = 1
+    # num_cols = marketing_df.dtypes[marketing_df.dtypes != 'object'].index.tolist()
+    # cat_cols = [col for col in marketing_df.dtypes[marketing_df.dtypes == 'object'].index.tolist() if col != target]
+    cols = [col for col in marketing_df.columns.tolist() if col != 'y']
+    col_types = [marketing_df[col].dtype.name for col in cols]
+    client_df = pd.DataFrame()
+    
+    for col, col_dtype in zip(cols,col_types):
+        if (col_dtype == 'object'):
+            col_option_lst = marketing_df[col].unique().tolist()
+            col_mode = marketing_df[col].mode().tolist()[0]
             
-    # if option_id == learner_option[0]:
-    # display_top_learner_report(report_df, nrows = 5)
-    # else:
-    # display_learner_report(option_id)
+            col_selected = col_option_lst.index(client_df_ok.iloc[tam][col])
+            col_option= st.selectbox('Choose '+col.capitalize()+':',options = col_option_lst, index = col_selected)  
+            client_df[col] = [col_option]
+        else:
+            min_val  = int(marketing_df[col].min())
+            max_val  = int(marketing_df[col].max())
+            value =  int(client_df_ok.iloc[tam][col])
+            col_option = st.slider(str(col), min_value= min_val, max_value= max_val, value=value, step=1)
+            
+            client_df[col] = [col_option]
 
-  
+    if st.button('Predict'):
+            st.write(client_df)
+            #client_df = client_df_ok .drop(['y'], axis = 1) 
+            client_df = process_input_client(client_df)
+            
+            X_test = client_df #StandardScaler().fit_transform(client_df)
+            
+            y_pred = model.predict(X_test)
+            st.write('yes' if y_pred == 1 else 'no')
+            
            
 main()
 
