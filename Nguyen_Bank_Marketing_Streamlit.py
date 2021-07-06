4751408b9c36e7d1d8d4aecd7a522198420e34ac
Hara Nguyen <ngttnguyen@gmail.com>
2021-07-06 23:34:27 +0700

    update

--- Nguyen_Bank_Marketing_Streamlit.py
+++ Nguyen_Bank_Marketing_Streamlit.py
@@ -1,11 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from pandas_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
-
 from sklearn.preprocessing import MinMaxScaler
-import json
 import pickle
 from matplotlib import pyplot as plt
 import seaborn as sns
@@ -18,9 +14,10 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 from sklearn.metrics import precision_recall_curve
 from sklearn import metrics
 from xgboost import XGBClassifier
-import scikitplot as skplt
+#import scikitplot as skplt
 import matplotlib.pyplot as plt
-
+import base64
+st.set_page_config(layout="wide")
 def transform_pdays(val):
     transform_dict = {999:'not_previously_contacted',7: 'over_a_week',0:'within_a_week'}
     for key in transform_dict.keys():
@@ -71,9 +68,8 @@ def get_cat_cols_val(data):
     return unique_dict
 
 def quick_predict_client():
-    client_df_ok = pd.read_csv("client_df.csv", index_col = 'Unnamed: 0')
+    client_df_ok = pd.read_csv("data/X_client_df.csv", index_col = 'Unnamed: 0')
 
-    st.markdown("Input client's information: ")
     target = 'y'
     tam = 1
     # num_cols = marketing_df.dtypes[marketing_df.dtypes != 'object'].index.tolist()
@@ -81,46 +77,56 @@ def quick_predict_client():
     cols = [col for col in marketing_df.columns.tolist() if col != 'y']
     col_types = [marketing_df[col].dtype.name for col in cols]
     client_df = pd.DataFrame()
-    
-    for col, col_dtype in zip(cols,col_types):
+    question_dict = {"age": "Enter age of the customer", "job": "Enter the type of job customer do","marital": "what is customer's marital status?", "education":"Enter customer's education level?",
+    "default": "Do the customer have credit in default?", "housing": "Do the customer have Housing Loan?", "loan": "Do the customer have Personal Loan?", "contact": "How do you prefer to communicate",
+    "month": "Which month the customer was last contacted in?", "day_of_week": "Which day of week the customer was contacted in", "duration": "Enter last contact duration with the customer in sec?",
+    "campaign": "Enter number of contacts performed during this campaign and for this client", "pdays": "Enter number of days that passed by after the client was last contacted from a previous campaign",
+    "previous": "Enter number of contacts performed before this campaign and for this client", "poutcome": "Enter outcome of the previous marketing campaign", "emp.var.rate": "Enter employment variation rate - quarterly indicator",
+    "cons.price.idx": "Enter consumer price index - monthly indicator", "cons.conf.idx": "Enter consumer confidence index - monthly indicator", "euribor3m": "Enter euribor 3 month rate - daily indicator", "nr.employed": "Enter number of employees - quarterly indicator"}
+    for col, col_dtype, key in zip(cols,col_types, question_dict):
         if (col_dtype == 'object'):
             col_option_lst = marketing_df[col].unique().tolist()
             col_mode = marketing_df[col].mode().tolist()[0]
             
             col_selected = col_option_lst.index(client_df_ok.iloc[tam][col])
-            col_option= st.selectbox('Choose '+col.capitalize()+':',options = col_option_lst, index = col_selected)  
+            col_option= st.selectbox(question_dict[key],options = col_option_lst, index = col_selected)  
             client_df[col] = [col_option]
         else:
             min_val  = int(marketing_df[col].min())
             max_val  = int(marketing_df[col].max())
             value =  int(client_df_ok.iloc[tam][col])
-            col_option = st.slider(str(col), min_value= min_val, max_value= max_val, value=value, step=1)
+            col_option = st.slider(question_dict[key], min_value= min_val, max_value= max_val, value=value, step=1)
             
             client_df[col] = [col_option]
 
-    if st.button('Predict'):
+    if st.button('Show Prediction'):
             st.write(client_df)
             #client_df = client_df_ok .drop(['y'], axis = 1) 
             client_df = process_input_client(client_df)
             X_test = scaler.transform(client_df)
             
             y_pred = model.predict(X_test)
-            result_str = 'high - potential' if y_pred == 1 else 'low - potential'
-            st.write('This is a '+ result_str + ' client for this tele marketing campain')
+            result_str = 'POTENTIAL' if y_pred == 1 else 'NON-POTENTIAL'
+            result ='This is a '+ result_str + ' customer for tele-marketing campaign'
+            if y_pred == 1:
+                st.success(result)
+            else:
+                st.warning(result)
 
 def visualize_predicted_result(df, target):
+    st.subheader("The Predicted Percentage Of Success:")
     data = df.groupby(target).size().sort_values(ascending=False)
     label_dict = {1:'yes',0:'no'}
-    fig = plt.figure(figsize = (4,3))
-    plt.pie(x=data , autopct="%.1f%%", explode=[0.05]*len(data), labels= [label_dict[val] for val in data.index.tolist()]);
-    plt.title("The predicted percentage of success");
-    st.pyplot(fig)  
+    fig = plt.figure(figsize = (3,3))
+    plt.pie(x=data , autopct='%.1f%%',  labels= [label_dict[val] for val in data.index.tolist()], pctdistance=0.7, radius=1.1)
+    col1, col2, col3 = st.beta_columns(3)
+    col2.pyplot(fig)   
     
 def predict_data_file(file):
     upload_data = get_df(file)
     features = [col for col in marketing_df.columns.tolist() if col != 'y']
     input_data  = upload_data[features]
-    st.markdown('Lis of clients to predict:')
+    st.subheader('List Of Customers To Predict:')
     st.write(input_data)
     
     ## prediction
@@ -133,13 +139,15 @@ def predict_data_file(file):
     ## summary result  
     pred_success_cnt = sum((y_client_pred == 1))
     total_cnt = len(y_client_pred)
-    st.markdown('Predicted result:')
-    st.markdown (str(pred_success_cnt) +" clients will say YES over "+str(total_cnt) +' people')
+    st.subheader('Predicted Result:')
+    st.success (str(pred_success_cnt) +" customers say YES over "+str(total_cnt) +' people')
     visualize_predicted_result(upload_data, result_col)
     
     ## view result
-    view_result_option = ['view all',"view successful list", "view unsuccesful list"]
-    view_type_id = st.selectbox('Choose view:',options = view_result_option)
+    st.subheader("Choose View:")
+    view_result_option = ['View All',"View Successful List", "View Unsuccesful List"]
+    col1, col2, col3, col4 = st.beta_columns(4)
+    view_type_id = col1.selectbox('',options = view_result_option)
     if (view_type_id == view_result_option[0]):
         st.write(upload_data) 
     else:
@@ -166,23 +174,66 @@ def get_df(file):
     df = pd.read_pickle(file)
   return df
 
+def view_models_summary():
+    st.write(metric_df)
+
 #### L O A D  Data
 data_file_path = "data/bank-additional-full.csv"
 marketing_df = pd.read_csv(data_file_path,sep = ";")
 
-model_file_path = "model/pkl_model.pkl"
-model = pickle.load(open(model_file_path, 'rb'))
-
 scaler_file_path = "model/pkl_scaler.pkl"
 scaler = pickle.load(open(scaler_file_path, 'rb'))
 
-#### M A I N  Function
+xgboost_clf_file_path = "model/pkl_xgboost_model.pkl"
+xgboost_clf = pickle.load(open(xgboost_clf_file_path, 'rb'))
+
+log_clf_file_path = "model/pkl_log_model.pkl"
+log_clf = pickle.load(open(log_clf_file_path, 'rb'))
+
+tree_clf_file_path = "model/pkl_decisionT_model.pkl"
+tree_clf = pickle.load(open(tree_clf_file_path, 'rb'))
+
+grboost_clf_file_path = "model/pkl_grboost_model.pkl"
+grboost_clf = pickle.load(open(grboost_clf_file_path, 'rb'))
+
+metric_file_path = "model/evaluation_metrics.csv"
+metric_df = pd.read_csv(metric_file_path)
+
+# Main function
 def main():
+
+    st.title("Bank Marketing Prediction")
     
-    st.title('Banking TeleMarketing prediction')
-    st.sidebar.markdown('## XGBoost Classifier')
-    predict_option = ['quick predict','predict on data file']
-    predict_type_id = st.sidebar.selectbox('Choose predict:',options = predict_option)
+    htk=  """
+    <div style="background-color:#004d99;padding:0px">
+    <h2 style="color:white;text-align:center;">CUSTOMER DESPOIT PREDICTION APP </h2>
+    </div>
+    """
+    st.markdown(htk,unsafe_allow_html=True)
+    
+    ## Summary models
+    st.sidebar.subheader('Summary models')
+    model_option = ["View models' summary",'Optimal Classifier','XGBoost Classifier','GradientBoost Classifier','Decision Tree Classifier','Logistic Regressor']
+    model_type_id = st.sidebar.selectbox('Choose model',options = model_option)
+    if(model_type_id == model_option[0]):
+        view_models_summary()
+    if (model_type_id in  model_option[1:3]):
+        model = xgboost_clf
+        st.write('XGBooost')
+    elif (model_type_id == model_option[3]):
+        model = grboost_clf
+        st.write('Gradient')
+    elif (model_type_id == model_option[4]):
+        model = tree_clf
+        st.write('Tree')
+    elif (model_type_id == model_option[5]):
+        model = log_clf
+        st.write('log')
+     
+        
+    ## Choose predict
+    predict_option = ['Quick Predict','Predict With Data File']
+    predict_type_id = st.sidebar.selectbox('Choose predict',options = predict_option)
     
     if (predict_type_id  == predict_option[0]):
         quick_predict_client()
@@ -192,7 +243,9 @@ def main():
             st.write("Upload a .csv or .xlsx file to get started")
         else:
             predict_data_file(file)
-           
+    if st.sidebar.button("Thanks") :
+        st.text("Thank you for visiting  and happy learning :)")
+        st.balloons()      
 main()
 
-## Run: streamlit run Nguyen_Bank_Marketing_Streamlit.py
+## Run: streamlit run apps/bank_marketing_streamlit.py
